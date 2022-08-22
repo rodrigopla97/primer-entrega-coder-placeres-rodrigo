db851e37deee718fe7f5d13660947a78f22a09dd
Rodrigo <rodrigoplaceres19@gmail.com>
2022-08-22 12:03:33 -0300

    Creacion de repo

---
+++ index.js
@@ -0,0 +1,19 @@
+// Se ingresan numeros y se suman los pares
+// se puede hacer con ifs
+
+let numero = 0;
+let suma = 0;
+
+alert("Ingresar numeros, se sumara los pares");
+
+numero = parseInt(prompt("Ingrese un numero, para finalizar ingrese 0"));
+console.log("Numero ingresado: " + numero);
+
+while (numero != 0) {
+  numero = parseInt(prompt("Ingrese un numero, para finalizar ingrese 0"));
+  if (numero % 2 === 0) {
+    suma += numero;
+  }
+  console.log("Numero ingresado: " + numero);
+}
+console.log("La suma de los pares es: " + suma);
